b0f1d2f9271b510f7ad2d78b7bf0e5393812a6a6
Hyukjin Kwon <pgkwon1@naver.com>
2022-09-05 23:19:28 +0900

    Move directory

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const {
     Client,
-    GatewayIntentBits
+    GatewayIntentBits,
+    EmbedBuilder
 } = require('discord.js');
 const Discord = require('discord.js'); // discord.js 라이브러리 호출
 const client = new Discord.Client({
@@ -16,19 +17,20 @@ const client = new Discord.Client({
 const prefix = "마법의소라고동님"
 const answerList = [
     "그래", "해도 돼", "그래 해도돼", "당연하지", "그래 좋아", "특별히 허락해주지", "안되는건 안되는거야", "당장 시작해", "당근이지",
-    "안돼", "하지마", "응 안돼", "나중에 해", "다음에 해", "어쩌라고", "절대 안돼", "안 들려", "그러지 마", "허락 못해", "그만해", "절대 하지 마"
+    "아니", "안돼", "하지마", "응 안돼", "나중에 해", "다음에 해", "어쩌라고", "절대 안돼", "안 들려", "그러지 마", "허락 못해", "그만해", "절대 하지 마"
 ]
 client.login(process.env.DISCORD_SECRET_KEY)
 client.on('ready', () => {
     console.log(`ready complete`)
 })
 
-client.on('messageCreate', msg => {  
+client.on('messageCreate', msg => {
     if (msg.author.bot) return false
     if (msg.content.startsWith(prefix)) {
         const randomNum = Math.floor(
             Math.random() * answerList.length
         )
+        console.log(randomNum)
         msg.reply(answerList[randomNum])
     }
 })
